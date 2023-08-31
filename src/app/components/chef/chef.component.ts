f798f9436dbb1877bda8051147eebe6558cf8a0b
Carol Protásio <protasio.carol@gmail.com>
2023-08-31 12:26:02 -0300

    varios

--- src/app/components/chef/chef.component.ts
+++ src/app/components/chef/chef.component.ts
@@ -2,18 +2,31 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth/auth.service';
 
+interface Order {
+  client: string;
+  table: string;
+  waiter: string;
+  status: string;
+  products: [];
+}
+
 @Component({
   selector: 'app-chef',
   templateUrl: './chef.component.html',
   styleUrls: ['./chef.component.css']
 })
 export class ChefComponent {
+  pendingOrders: any[] = [];
+  completedOrders: any[] = [];
+  showPendingOrders: boolean = true;
+  showImage: boolean = false; 
 
   constructor(private router: Router, private authService: AuthService) {}
 
   logout() {
     this.authService.logout();
   }
+  
 
 
 }
--- src/app/components/order/order.component.ts
+++ src/app/components/order/order.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, ChangeDetectorRef, Output, EventEmitter  } from '@angular/core';
+import { Component, Input, ChangeDetectorRef, Output, EventEmitter } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-order',
@@ -9,12 +10,15 @@ export class OrderComponent {
   @Input() listProduct: any[] = [];
   @Input() totalValue: number = 0;
   orderProducts: any[] = [];
-  loggedInUsername ="loggedInUsername"
+  @Input() loggedInUsername: string = '';
   @Output() sendOrder: EventEmitter<any> = new EventEmitter<any>();
-  
+  customerName: string = '';
+  successMessageVisible = false;
+  successMessage = '';
 
   constructor(
-    private cdRef: ChangeDetectorRef
+    private cdRef: ChangeDetectorRef,
+    private http: HttpClient
   ) {}
 
   ngOnChanges(changes: any) {
@@ -44,19 +48,74 @@ export class OrderComponent {
 
   addToOrder() {
     if (this.listProduct && this.listProduct.length > 0) {
-      this.orderProducts.push(...this.listProduct);
-      this.listProduct = [];
-      this.orderProducts = [];
-      console.log('Order Products:', this.orderProducts);
-      this.calculateTotalValue();
-      this.cdRef.detectChanges();      
+      const token = localStorage.getItem('token');
+      this.customerName = (document.getElementById('customerName') as HTMLInputElement).value;
+
+      if (token) {
+        const headers = new HttpHeaders({
+          Authorization: `Bearer ${token}`
+        });
+
+        const order = {
+          waiter: this.loggedInUsername,
+          client: this.customerName,
+          status: 'pending',
+          completedDate: '',
+          orderDelivered: '',
+          products: this.listProduct.map(item => {
+            return {
+              name: item.product.name,
+              quantity: item.quantity
+            };            
+          })
+        };
+
+        this.http.post('http://localhost:8080/orders', order, { headers }).subscribe(
+          (response: any) => {
+            console.log('Pedido enviado com sucesso:', response);
+            /* this.orderProducts = []; */
+            this.customerName = '';
+            this.totalValue = 0;
+            this.cdRef.detectChanges();
+            
+            this.sendOrder.emit();
+            
+            console.log(this.orderProducts)
+            console.log(this.listProduct)
+            this.calculateTotalValue();
+            this.openSuccessMessage();
+
+            
+          },
+          (error: any) => {
+            console.error('Erro ao enviar pedido para a API:', error);
+          }
+        );
+      } else {
+        console.error('Token de autenticação não encontrado.');
+      }
+    }
   }
+
+  resetOrder() {
+    this.listProduct = [];
+    this.totalValue = 0;
+  }
+
+  openSuccessMessage() {
+    this.successMessageVisible = true;
+    this.successMessage = 'Pedido enviado com sucesso!';
+
+    setTimeout(() => {
+      this.successMessageVisible = false;
+      this.successMessage = '';
+      this.resetOrder();
+    }, 1000);
   }
 
   calculateTotalValue() {
     this.totalValue = this.listProduct.reduce((total: number, product: any) => {
       return total + product.qty * product.product.price;
-      
     }, 0);
   }
 }
